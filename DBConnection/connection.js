a69c7cdc6993a13a5d7769a9ae2c680c57c26112
Thalis Trisch <thalis.trisch2003@gmail.com>
2023-05-06 15:45:15 -0300

    mongo connect bug fixed

--- DBConnection/connection.js
+++ DBConnection/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
+const user = "thalistrisch2003"
+const password = "Qi8TRb4WUjnxHISy"
 async function Start(){
     try{
-        const db = mongoose.connect('mongodb+srv://thalistrisch:q6J2sTOG465yzGSm@cluster0.knpmfxp.mongodb.net/test')
+        const db = mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.b9ya9is.mongodb.net/`)
         console.log("conectado ao banco.")
     }catch(err){
         console.log("Erro no banco: "+err)
